a8b3a942e15f1f0b04c8a94922d647539a1ce940
aydenl3 <soloyeti41@gmail.com>
2024-11-18 12:32:18 -0800

    D3.C Completed: Player Motion Implemented

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,14 @@
 import leaflet from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
-// Fix missing marker images
 import "./leafletWorkaround.ts";
-// Deterministic random number generator
 import luck from "./luck.ts";
 
-const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
-console.log("hellow wolr");
-
-// Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(document.getElementById("map")!, {
@@ -48,10 +43,29 @@ interface Cell {
   readonly j: number;
 }
 
-interface Cache {
-  num_coins: number;
+interface Momento<T> {
+  toMomento(): T;
+  fromMomento(momento: T): void;
+}
+
+class Cache implements Momento<string> {
   location: Cell;
   coins: Map<string, Coin>;
+
+  constructor(location: Cell) {
+    this.location = location;
+    this.coins = new Map<string, Coin>();
+  }
+
+  toMomento() {
+    const mapAsArray = Array.from(this.coins.entries());
+    return JSON.stringify(mapAsArray);
+  }
+
+  fromMomento(momento: string) {
+    const mapAsArray: [string, Coin][] = JSON.parse(momento);
+    this.coins = new Map<string, Coin>(mapAsArray);
+  }
 }
 
 interface Coin {
@@ -65,20 +79,20 @@ class Board {
   readonly tileVisibilityRadius: number;
 
   private readonly knownCells: Map<string, Cell>;
-  private readonly knownCaches: Map<string, Cache>;
+  public readonly knownCaches: Map<string, Cache>;
+  public readonly cacheData: Map<string, string>;
 
   constructor(tileWidth: number, tileVisibilityRadius: number) {
     this.tileWidth = tileWidth;
     this.tileVisibilityRadius = tileVisibilityRadius;
     this.knownCells = new Map<string, Cell>();
     this.knownCaches = new Map<string, Cache>();
+    this.cacheData = new Map<string, string>();
   }
 
   private getCanonicalCell(cell: Cell): Cell {
     const { i, j } = cell;
     const key = [i, j].toString();
-
-    // if cell doesnt exist, make a new one and add it to canonical cells
     if (!this.knownCells.get(key)) {
       const newcell: Cell = { i: i, j: j };
       this.knownCells.set(key, newcell);
@@ -87,7 +101,6 @@ class Board {
   }
 
   getCellForPoint(point: leaflet.LatLng): Cell {
-    // find cell from point
     const i = point.lat;
     const j = point.lng;
     const newcell: Cell = { i: i, j: j };
@@ -95,32 +108,19 @@ class Board {
   }
 
   getCellBounds(cell: Cell): leaflet.LatLngBounds {
-    // ...
     const cellBounds = leaflet.latLngBounds([
       [cell.i, cell.j],
       [cell.i + (1 * this.tileWidth), cell.j + (1 * this.tileWidth)],
     ]);
     return cellBounds;
   }
-  //not yet functioning
-  /*
-    getCellsNearPoint(point: leaflet.LatLng): Cell[] {
-        const resultCells: Cell[] = [];
-        const originCell = this.getCellForPoint(point);
-        // ...
-        return resultCells;
-    }
-        */
-  getCache(cell: Cell) {
+
+  newCache(cell: Cell) {
     const { i, j } = cell;
     const key = [i, j].toString();
     if (!this.knownCaches.get(key)) {
       const numCoins = Math.floor(luck([i, j, "initialValue"].toString()) * 10);
-      const newcache: Cache = {
-        num_coins: numCoins,
-        location: cell,
-        coins: new Map<string, Coin>(),
-      };
+      const newcache = new Cache(cell);
       for (let k = 0; k < numCoins; k++) {
         const newCoin: Coin = {
           location: cell,
@@ -130,23 +130,40 @@ class Board {
         newcache.coins.set(newCoin.name, newCoin);
       }
       this.knownCaches.set(key, newcache);
+      const newdata = newcache.toMomento();
+      this.cacheData.set(key, newdata);
     }
-    return this.knownCaches.get(key)!;
+    const cache = this.knownCaches.get(key)!;
+    const data = this.cacheData.get(key)!;
+    cache.fromMomento(data);
+    return cache;
+  }
+
+  getCache(cell: Cell) {
+    const { i, j } = cell;
+    const key = [i, j].toString();
+    const cache = this.knownCaches.get(key);
+    if (cache) {
+      const data = this.cacheData.get(key)!;
+      cache.fromMomento(data);
+      return cache;
+    }
   }
 }
+const activeRectangles: leaflet.Rectangle[] = [];
 function drawCache(cache: Cache) {
   const bounds = userBoard.getCellBounds(cache.location);
   const rect = leaflet.rectangle(bounds);
   rect.addTo(map);
+  activeRectangles.push(rect);
   rect.bindPopup(() => {
     const popupDiv = document.createElement("div");
     const buttonsDiv = document.createElement("div");
     buttonsDiv.id = "button-container";
 
     popupDiv.innerHTML = `
-          <div>There is a cache here at "${cache.location.i},${cache.location.j}". It has value <span id="value">${cache.num_coins}</span>.</div>`;
+          <div>There is a cache here at "${cache.location.i},${cache.location.j}". It has value <span id="value">${cache.coins.size}</span>.</div>`;
 
-    // Create buttons based on the number of coins
     for (const coin of cache.coins.values()) {
       const button = document.createElement("button");
       button.textContent = `Take coin: ${coin.name}`;
@@ -163,7 +180,6 @@ function drawCache(cache: Cache) {
       buttonsDiv.appendChild(button);
     }
 
-    // Optionally, add a single drop button if applicable
     const dropButton = document.createElement("button");
     dropButton.textContent = "Drop";
     dropButton.addEventListener(
@@ -175,10 +191,7 @@ function drawCache(cache: Cache) {
         ),
     );
     buttonsDiv.appendChild(dropButton);
-
-    // Append the button container to the popupDiv
     popupDiv.appendChild(buttonsDiv);
-
     return popupDiv;
   });
 }
@@ -189,16 +202,19 @@ function handleTakeClick(
   button: HTMLButtonElement,
   valueSpan: HTMLSpanElement,
 ) {
-  if (cache.num_coins > 0) {
+  if (cache.coins.size > 0) {
     const this_coin = cache.coins.get(coin.name);
     if (this_coin) {
       cache.coins.delete(coin.name);
+      userBoard.cacheData.set(
+        [cache.location.i, cache.location.j].toString(),
+        cache.toMomento(),
+      );
       playerWallet.push(this_coin);
-      cache.num_coins--;
       playerCoins++;
       button.textContent = `Took coin: ${coin.serial}`;
     }
-    valueSpan.innerHTML = cache.num_coins.toString();
+    valueSpan.innerHTML = cache.coins.size.toString();
     statusPanel.innerHTML = `${playerCoins} Coins accumulated: <br>`;
     for (const coinin of playerWallet) {
       statusPanel.innerHTML += coinin.name;
@@ -212,10 +228,13 @@ function handleDropClick(cache: Cache, valueSpan: HTMLSpanElement) {
     const dropped_coin = playerWallet.pop();
     if (dropped_coin) {
       cache.coins.set(dropped_coin.name, dropped_coin);
-      cache.num_coins++;
+      userBoard.cacheData.set(
+        [cache.location.i, cache.location.j].toString(),
+        cache.toMomento(),
+      );
       playerCoins--;
     }
-    valueSpan.innerHTML = cache.num_coins.toString();
+    valueSpan.innerHTML = cache.coins.size.toString();
     statusPanel.innerHTML = `${playerCoins} Coins accumulated: <br>`;
     for (const coinin of playerWallet) {
       statusPanel.innerHTML += coinin.name;
@@ -224,23 +243,84 @@ function handleDropClick(cache: Cache, valueSpan: HTMLSpanElement) {
   }
 }
 
-const userBoard: Board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
-const playerLocation = OAKES_CLASSROOM; //THIS WILL CHANGE WHEN MOVEMENT IS ADDED
-let playerCoins: number = 0;
-const playerWallet: Coin[] = [];
+function initializeControlPanel() {
+  const northButton = document.getElementById("north")!;
+  const southButton = document.getElementById("south")!;
+  const westButton = document.getElementById("west")!;
+  const eastButton = document.getElementById("east")!;
 
-// Look around the player's neighborhood for caches to spawn
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    const newpoint = leaflet.latLng(
-      playerLocation.lat + i * TILE_DEGREES,
-      playerLocation.lng + j * TILE_DEGREES,
-    );
-    const newcell = userBoard.getCellForPoint(newpoint);
-    //If location i,j is lucky enough, spawn a cache!
-    if (luck([newcell.i, newcell.j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      const newCache = userBoard.getCache(newcell);
-      drawCache(newCache);
+  northButton.addEventListener("click", () => {
+    navigate("north");
+  });
+  southButton.addEventListener("click", () => {
+    navigate("south");
+  });
+  westButton.addEventListener("click", () => {
+    navigate("west");
+  });
+  eastButton.addEventListener("click", () => {
+    navigate("east");
+  });
+}
+
+function navigate(direction: string) {
+  console.log(`Navigating ${direction}`);
+  if (direction == "north") {
+    playerLocation.lat += 0.0001;
+    map.panTo(playerLocation);
+    playerMarker.setLatLng(playerLocation);
+    clearRectangles();
+    generateCaches();
+  } else if (direction == "south") {
+    playerLocation.lat -= 0.0001;
+    map.panTo(playerLocation);
+    playerMarker.setLatLng(playerLocation);
+    clearRectangles();
+    generateCaches();
+  } else if (direction == "west") {
+    playerLocation.lng -= 0.0001;
+    map.panTo(playerLocation);
+    playerMarker.setLatLng(playerLocation);
+    clearRectangles();
+    generateCaches();
+  } else if (direction == "east") {
+    playerLocation.lng += 0.0001;
+    map.panTo(playerLocation);
+    playerMarker.setLatLng(playerLocation);
+    clearRectangles();
+    generateCaches();
+  }
+}
+function clearRectangles() {
+  activeRectangles.forEach((rect) => map.removeLayer(rect));
+  activeRectangles.length = 0;
+}
+
+function generateCaches() {
+  for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+    for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+      const newpoint = leaflet.latLng(
+        (playerLocation.lat + i * TILE_DEGREES).toFixed(4),
+        (playerLocation.lng + j * TILE_DEGREES).toFixed(4),
+      );
+      const newcell = userBoard.getCellForPoint(newpoint);
+      const maybe_cache = userBoard.getCache(newcell);
+      if (maybe_cache) {
+        drawCache(maybe_cache);
+      } else {
+        if (luck([newcell.i, newcell.j].toString()) < CACHE_SPAWN_PROBABILITY) {
+          console.log(luck([newcell.i, newcell.j].toString()));
+          const newCache = userBoard.newCache(newcell);
+          drawCache(newCache);
+        }
+      }
     }
   }
 }
+
+const userBoard: Board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
+const playerLocation = OAKES_CLASSROOM;
+let playerCoins: number = 0;
+const playerWallet: Coin[] = [];
+initializeControlPanel();
+generateCaches();
